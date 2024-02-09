6c57599505138f3156ce37e076e8c13c8b42d780
Houman <veryhouman@gmail.com>
2024-02-09 09:21:53 -0500

    Initial Commit

---
+++ ThriftMe/ThriftMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriftMeApp.swift
+//  ThriftMe
+//
+//  Created by Houman Azari on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct ThriftMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
